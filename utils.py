a3c2e367f9dd74e7cbad44bd3210789eb8efe1e1
shabkr <95105822+shabkr@users.noreply.github.com>
2025-05-19 20:59:02 -0600

    add sim_eeg_mne function for simulating eeg that is in the MNE Epochs array

--- utils.py
+++ utils.py
@@ -82,6 +82,91 @@ def plot_simEEG(*args):
 
     plt.show()
     
+def plot_simEEG_mne(*args):
+  """
+  plot_simEEG - plot function for MXC's course on neural time series analysis
+  """
+  if not args:
+      raise ValueError('No inputs!')
+  elif len(args) == 1:
+      EEG = args[0]
+      chan, fignum = 0, 0
+  elif len(args) == 2:
+      EEG, chan = args
+      fignum = 0
+  elif len(args) == 3:
+      EEG, chan, fignum = args
+
+  plt.figure(fignum, figsize=(16,10))
+  plt.clf()
+
+  # some convenience variables from EEG
+  srate = EEG.info['sfreq']
+  pnts = len(EEG.times)
+  data = EEG.get_data() # epochs,channels,times
+  data = np.transpose(data, axes = (1,2,0)) #channels, times, epochs
+  trials = len(EEG.events)
+
+  # ERP
+  plt.subplot(211)
+  plt.plot(EEG.times, np.squeeze(data[chan,:,:]), linewidth=0.5, color=[.75, .75, .75])
+  plt.plot(EEG.times, np.squeeze(np.mean(data[chan,:,:], axis=1)), 'k', linewidth=3)
+  plt.xlabel('Time (s)')
+  plt.ylabel('Activity')
+  plt.title(f'ERP from channel {chan}')
+
+  # static power spectrum
+  hz = np.linspace(0, srate, pnts)
+  if len(data.shape) == 3:
+      pw = np.mean((2 * np.abs(fft(data[chan,:,:], axis=0) / pnts))**2, axis=1)
+  else:
+      pw = (2 * np.abs(fft(data[chan,:], axis=0) / pnts))**2
+
+  plt.subplot(223)
+  plt.plot(hz, pw, linewidth=2)
+  plt.xlim([0, 40])
+  plt.xlabel('Frequency (Hz)')
+  plt.ylabel('Power')
+  plt.title('Static power spectrum')
+
+  # time-frequency analysis
+  frex = np.linspace(2, 30, 40)  # frequencies in Hz (hard-coded to 2 to 30 in 40 steps)
+  waves = 2 * (np.linspace(3, 10, len(frex)) / (2 * np.pi * frex))**2  # number of wavelet cycles (hard-coded to 3 to 10)
+
+  # setup wavelet and convolution parameters
+  wavet = np.arange(-2, 2, 1/srate)
+  halfw = len(wavet) // 2
+  nConv = pnts * trials + len(wavet) - 1
+
+  # initialize time-frequency matrix
+  tf = np.zeros((len(frex), pnts))
+
+  # spectrum of data
+  dataX = fft(np.reshape(data[chan,:,:], -1, order='F'), n=nConv)
+  # loop over frequencies
+  for fi in range(len(frex)):
+      # create wavelet
+      waveX = fft(np.exp(2 * 1j * np.pi * frex[fi] * wavet) * np.exp(-wavet**2 / waves[fi]), n=nConv)
+      waveX = waveX / np.max(waveX) # normalize
+      
+      # convolve
+      as_ = ifft(waveX * dataX)
+      # trim and reshape
+      as_ = np.reshape(as_[halfw:len(as_)-halfw+1], [pnts, trials], order='F')
+
+      # power
+      tf[fi, :] = np.mean(np.abs(as_), axis=1)
+
+  # show a map of the time-frequency power
+  plt.subplot(224)
+  plt.contourf(EEG.times, frex, tf, 40, cmap='jet')
+  plt.xlabel('Time')
+  plt.ylabel('Frequency (Hz)')
+  plt.title('Time-frequency plot')
+
+  plt.show()
+
+
 def topoPlotIndie(eeg, values, title='Topoplot', vlim=(None, None), cmap='RdBu_r', contours=6):
     '''
         eeg = eeg mat file loaded
